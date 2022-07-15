fbe412233cea2775998fd1f9c6ea3b6074d2f538
isybycnsmam <maciej-scholz@tlen.pl>
2022-07-15 21:28:23 +0100

    Fixed deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,13 +4,14 @@ echo "Stopping proxy-gc service"
 sudo systemctl stop proxy-gc
 
 echo "Removing publish folder if exists"
-if [ -d "../src/publish" ]
+if [ -d "../publish" ]
 then
-    rm -r ../src/publish
+    rm -r ../publish
 fi
 
 echo "Publishing"
-dotnet publish -c Release -r linux-arm64 -o ../src/publish --no-self-contained
+cd ../src/ProxyService
+dotnet publish -c Release -r linux-arm64 -o ../../publish --no-self-contained
 
 echo "Starting proxy-gc service"
 sudo systemctl start proxy-gc
